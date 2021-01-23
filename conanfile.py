e456cbfe0f0c305925fdf911269edfd78d89f3ea
lnikon <bejanyan.vahag@protonmail.com>
2021-01-23 11:17:26 +0400

    DevOps: Support for conan. Added Boost ans JSON deps

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+from conans import ConanFile
+
+class ConanPackage(ConanFile):
+    name = 'network-monitor'
+    version = "0.1.0"
+
+    generators = 'cmake_find_package'
+
+    requires = [
+        ('boost/1.74.0'),
+        ('nlohmann_json/3.9.1')
+    ]
+
+    default_options = (
+        'boost:shared=False',
+    )
